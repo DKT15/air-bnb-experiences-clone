14967f5e90736796696218315707b528db8533ba
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-08-25 20:14:21 +0100

    added to the code.

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,10 @@ import data from "./data.js";
 function App() {
   // using map to map over all of the data in the js file and apply it to the card prop that has been created.
   const cards = data.map((item) => {
-    return <Card key={item.id} {...item} />;
+    return <Card key={item.id} {...item} />; //spread operator is used to map over all of the items in the card rather than writing each item out individually.
   });
 
+  // Linking to cards in the return below.
   return (
     <>
       <Navbar />
